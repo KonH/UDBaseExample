57d17d6a2757bb0de1ef817e1f4b5e4167d61b31
KonH <konh@yandex.ru>
2016-10-30 21:09:10 +0700

    Scene setup;

--- Assets/Scripts/Scene/LoadExampleSceneButton.cs
+++ Assets/Scripts/Scene/LoadExampleSceneButton.cs
@@ -1,18 +1,8 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using UDBase.Controllers.Scene.UI;
 using UDBase.Controllers.Scene;
 
 [RequireComponent(typeof(Button))]
-public class LoadExampleSceneButton : MonoBehaviour {
-	public string ExampleName = "";
-
-	void Start () {
-		GetComponent<Button>().onClick.AddListener(OnClick);
-	}
-	
-
-	void OnClick () {
-		Scene.LoadScene("Example", ExampleName);
-	}
-}
+public class LoadExampleSceneButton : SceneParamLoadButton<Scenes> {}
---
+++ Assets/Scripts/Scene/Scenes.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum Scenes {
+	MainScene,
+	Example
+}
