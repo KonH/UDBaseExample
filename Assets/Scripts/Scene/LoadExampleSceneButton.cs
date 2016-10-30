23d550e4fe1c06ee9d9c7e1c7d2d046942d96663
KonH <konh@yandex.ru>
2016-10-30 12:05:32 +0700

    Example scene loader; Scene name format;

---
+++ Assets/Scripts/Scene/LoadExampleSceneButton.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UDBase.Controllers.Scene;
+
+[RequireComponent(typeof(Button))]
+public class LoadExampleSceneButton : MonoBehaviour {
+	public string ExampleName = "";
+
+	void Start () {
+		GetComponent<Button>().onClick.AddListener(OnClick);
+	}
+	
+
+	void OnClick () {
+		Scene.LoadScene("Example", ExampleName);
+	}
+}
