1f8bba3f2f44dd58e447fcd8141c6e6c4776b23e
KonH <konh@yandex.ru>
2016-11-23 22:38:50 +0700

    Info raw proto;

---
+++ Assets/Scripts/Test/InfoTest.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UDBase.Controllers.InfoSystem;
+
+public class InfoTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		var data = Info.GetInfo<TestData>("123");
+		Debug.Log(data.Data);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/UDBase_Project/Schemes/TestScheme.cs
+++ Assets/UDBase_Project/Schemes/TestScheme.cs
@@ -7,6 +7,7 @@ using UDBase.Controllers.ConfigSystem;
 using UDBase.Controllers.SaveSystem;
 using UDBase.Controllers.SceneSystem;
 using UDBase.Controllers.InventorySystem;
+using UDBase.Controllers.InfoSystem;
 
 public class ProjectScheme : Scheme {
 
@@ -17,6 +18,10 @@ public class ProjectScheme : Scheme {
 		AddController(new StateExample(), new ConcreteStateExample());
 		AddController(new Scene(), new AsyncSceneLoader("Loading", "MainScene"));
 		AddController(new Inventory(), new SimpleInventory());
+
+		var infos = new IInfoBase[1];
+		infos[0] = new ConfigInfoHolder<TestData>();
+		AddController(new Info(), infos);
 	}
 }
 #endif
