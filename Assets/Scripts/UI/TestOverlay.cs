24991f91b79cbbd68514b0c07a25dee39ef55109
KonH <konh@yandex.ru>
2017-02-07 20:46:52 +0700

    Overlay example;

---
+++ Assets/Scripts/UI/TestOverlay.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UDBase.UI.Common;
+
+public class TestOverlay : MonoBehaviour {
+
+	public int MaxDepth = 3;
+	public Button MoreButton = null;
+	public UIElement NextOverlay = null;
+	public Vector3 Offset;
+
+	void Start () {
+		var curDepth = UIManager.Current.OverlayDepth;
+		if( curDepth < MaxDepth ) {
+			MoreButton.onClick.AddListener(() => OnClick());
+		} else {
+			MoreButton.interactable = false;
+		}
+		transform.localPosition += Offset * curDepth;
+	}
+	
+	void OnClick() {
+		var go = Instantiate(NextOverlay.gameObject) as GameObject;
+		UIManager.Current.ShowOverlay(go.GetComponent<UIElement>());
+	}
+}
