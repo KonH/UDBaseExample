59209681da1ad086f1354e2d3eff16cef40b4874
KonH <konh@yandex.ru>
2017-02-21 20:59:38 +0700

    UpdateHelper test and examples;

---
+++ Assets/Scripts/UpdateHelper/CustomUpdateExample.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UDBase.Utils;
+
+public class CustomUpdateExample : MonoBehaviour, ICustomUpdate
+{
+	void OnEnable() {
+		UpdateHelper.SceneSubscribe(this);
+	}
+
+	void OnDisable() {
+		UpdateHelper.SceneUnsubscribe(this);
+	}
+
+    public void CustomUpdate() {
+	}
+}
---
+++ Assets/Scripts/UpdateHelper/TestSpawner.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class TestSpawner : MonoBehaviour {
+	public int Count = 10000;
+	public bool Custom = false;
+
+	void Start () {
+		for( int i = 0; i < Count; i++ ) {
+			var go = new GameObject("test");
+			if( Custom ) {
+				go.AddComponent<CustomUpdateExample>();
+			} else {
+				go.AddComponent<UpdateExample>();
+			}
+		}	
+	}
+}
