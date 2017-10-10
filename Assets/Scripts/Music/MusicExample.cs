5b88063ca0fffc8e7733d64d2c6c66be2dac64c4
KonH <konh@yandex.ru>
2017-10-10 20:20:24 +0700

    Music example controls;

---
+++ Assets/Scripts/Music/MusicExample.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UDBase.Controllers.MusicSystem;
+
+public class MusicExample : MonoBehaviour {
+
+	public void Pause() {
+		Music.Pause();
+	}
+
+	public void UnPause() {
+		Music.UnPause();
+	}
+}
