801f65147e9548c329885bdd62491da8e1f26665
KonH <konh@yandex.ru>
2018-02-11 17:52:53 +0700

    Updated example for localization;

--- Assets/UDBase_Project/Scripts/Installers/ExampleInstaller.cs
+++ Assets/UDBase_Project/Scripts/Installers/ExampleInstaller.cs
@@ -19,7 +19,8 @@ public class ExampleInstaller : UDBaseInstaller {
 	public SaveAudioController.Settings SaveAudioSettings;
 	public SoundUtility.Settings SoundSettings;
 	public WebLeaderboard.Settings WebLeaderboardSettings;
-	public SingleLocaleParser.Settings LocalizationSettings;
+	public SingleLocaleParser.Settings LocalizationParserSettings;
+	public Localization.Settings LocalizationSettings;
 
 
 	public override void InstallBindings() {
@@ -58,7 +59,8 @@ public class ExampleInstaller : UDBaseInstaller {
 		AddLocalTime();
 		AddSaveUser();
 		AddLocalLeaderboard();
-		AddSingleFileLocalization(LocalizationSettings);
+		AddSingleFileLocalizationParser(LocalizationParserSettings);
+		AddSaveLocalization(LocalizationSettings);
 
 		// Project-specific controller
 		Container.Bind<ConcreteStateExample>().AsSingle();
