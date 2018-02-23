a13e9ff3b4af2971e7a502d62d391fb8f039fbad
KonH <konh@yandex.ru>
2018-02-23 21:54:32 +0700

    Updated OneLine, example to reproduce issue;

---
+++ Assets/Example.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UDBase.Controllers.LogSystem;
+using Rotorz.Games.Reflection;
+using OneLine;
+
+public class Example : MonoBehaviour {
+
+	[System.Serializable]
+	public class ExampleItem {
+		[ClassImplements(typeof(ILogContext))]
+		public ClassTypeReference Reference;
+		public bool Field;
+	}
+
+	[OneLine]
+	public List<ExampleItem> Items;
+}
