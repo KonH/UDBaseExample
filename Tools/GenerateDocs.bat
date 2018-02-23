6ff1735a373da05d6b2fdab027a9d163301245b9
KonH <konh@yandex.ru>
2018-02-23 15:10:18 +0700

    Updated doc generation;

--- Tools/GenerateDocs.bat
+++ Tools/GenerateDocs.bat
@@ -1,1 +1,1 @@
-MarkdownGenerator.exe ../Temp/UnityVS_bin/Debug/Assembly-CSharp.dll ../Wiki UDBase
+MarkdownGenerator.exe ../Temp/UnityVS_bin/Debug/UDBase.Scripts.dll ../Wiki UDBase
--- Tools/GenerateDocs.sh
+++ Tools/GenerateDocs.sh
@@ -1,1 +1,1 @@
-mono MarkdownGenerator.exe ../Temp/UnityVS_bin/Debug/Assembly-CSharp.dll ../Wiki UDBase
+mono MarkdownGenerator.exe ../Temp/UnityVS_bin/Debug/UDBase.Scripts.dll ../Wiki UDBase
